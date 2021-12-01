0e15fa4b26fdcb9640b1540d4a664812664a9933
glowfi <86939591+glowfi@users.noreply.github.com>
2021-12-02 02:30:44 +0530

    fixed script

--- sYT.sh
+++ sYT.sh
@@ -9,5 +9,5 @@ function jsonArrayToTable(){
      jq -r '(.[0] | ([keys[] | .] |(., map(length*"-")))), (.[] | ([keys[] as $k | .[$k]])) | @tsv' | column -t -s $'\t'   
 }
 
-python __init__.py -q "$query";
+python sYT.py -q "$query";
 cat ~/data.json | jsonArrayToTable |fzf --height 20 --reverse | awk '{print $NF}'|xargs -t -I {} mpv "https://youtube.com{}"
