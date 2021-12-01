d521a84ffe8b3a64746d28bfb4b85fcff35616af
glowfi <86939591+glowfi@users.noreply.github.com>
2021-12-01 22:43:27 +0530

    Added files

---
+++ syt.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+read -p "Search : " query
+echo "Scraping ......"
+echo ""
+echo ""
+
+function jsonArrayToTable(){
+     jq -r '(.[0] | ([keys[] | .] |(., map(length*"-")))), (.[] | ([keys[] as $k | .[$k]])) | @tsv' | column -t -s $'\t'   
+}
+
+python ~/ytSearch/__init__.py -q "$query";
+cat data.json | jsonArrayToTable |fzf --height 20 --reverse | awk '{print $NF}'|xargs -t -I {} mpv "https://youtube.com{}"
