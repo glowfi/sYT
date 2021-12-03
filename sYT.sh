e25ac13b19627327a44a360e6c8428b18af36a48
glowfi <86939591+glowfi@users.noreply.github.com>
2021-12-04 02:13:01 +0530

    added features dmenu

--- sYT.sh
+++ sYT.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
+# Progress bar
+function progress ()
+{    
+    # Start Scraping data
+    BLUE='\033[0;34m'
+    echo ""
+    echo -e "$BLUE Scraping ......"
+    echo ""
+    sleep 1
+    clear
 
-# Read user query
-read -p $'\e[31mSearch query\e[0m :' query
-
-# Start Scraping data
-BLUE='\033[0;34m'
-echo ""
-echo -e "$BLUE Scraping ......"
-echo ""
-sleep 1
-clear
-
-# Start populating results
-ORANGE='\033[0;33m'
-echo -e "$ORANGE === Showing results for $query ==="
-echo ""
+    # Start populating results
+    ORANGE='\033[0;33m'
+    echo -e "$ORANGE === Showing results for $query ==="
+    echo ""
+}
 
 # Pretty print data
 function jsonArrayToTable(){
@@ -23,6 +23,28 @@ function jsonArrayToTable(){
 }
 
 # Execute script
-python ~/.local/bin/sYT.py -q "$query";
-cat ~/data.json | jsonArrayToTable |fzf --prompt="Find :" --cycle --height 20 --reverse | awk '{print $NF}'|xargs -t -I {} mpv "{}"
+
+if [[ "$1" ]]; then
+
+    # Read user query
+    query=$(echo >2 |dmenu -p "Search query :" -nb "#32302f" -nf "#bbbbbb" -sb "#477D6F" -sf "#eeeeee")
+
+    if [[ "$query" ]]; then
+    python ~/.local/bin/sYT.py -q "$query";
+    cat ~/data.json | jsonArrayToTable |dmenu -l 20 -p "Find:" -i -nb "#32302f" -nf "#bbbbbb" -sb "#477D6F" -sf "#eeeeee" | awk '{print $NF}'|xargs -t -I {} mpv "{}"
+    fi
+
+    
+else 
+    # Read user query
+    read -p $'\e[31mSearch query\e[0m :' query
+
+    # Show progress 
+    progress
+
+    python ~/.local/bin/sYT.py -q "$query";
+    cat ~/data.json | jsonArrayToTable |fzf --prompt="Find :" --cycle --height 20 --reverse | awk '{print $NF}'|xargs -t -I {} mpv "{}"
+fi
+
+
 rm -rf ~/argparse ~/json ~/os ~/requests ~/urllib.parse ~/data.json
