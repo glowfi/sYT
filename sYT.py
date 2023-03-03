e364e091bedddab70d35e22d4cde82966ea23cba
glowfi <86939591+glowfi@users.noreply.github.com>
2023-03-03 22:05:41 +0530

    FUTURE RELEASE

--- sYT.py
+++ sYT.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import urllib.request
 import time
+from numerize import numerize
 
 
 class searchYouTube:
@@ -12,8 +13,31 @@ class searchYouTube:
     def __init__(self, *args):
         self.search_term = args[0]
         self.max_results = args[1]
+        self.algo = args[2]
+
         self.BASE_URL = "https://youtube.com"
-        self.videos = self.sendRequest()
+
+        if self.algo == "v1":
+            self.videos = self.sendRequest()
+        self.videos = self.parseV2()
+
+    def human_time_duration(self, seconds):
+        TIME_DURATION_UNITS = (
+            ("week", 60 * 60 * 24 * 7),
+            ("day", 60 * 60 * 24),
+            ("hour", 60 * 60),
+            ("min", 60),
+            ("sec", 1),
+        )
+
+        if seconds == 0:
+            return "inf"
+        parts = []
+        for unit, div in TIME_DURATION_UNITS:
+            amount, seconds = divmod(int(seconds), div)
+            if amount > 0:
+                parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
+        return ", ".join(parts)
 
     def sendRequest(self):
         encoded_search = self.search_term.split(" ")
@@ -22,7 +46,7 @@ class searchYouTube:
         response = requests.get(url).text
         while "ytInitialData" not in response:
             response = requests.get(url).text
-        results = self.parseHTML(response)
+        results = self.parseV1(response)
         if self.max_results is not None and len(results) > self.max_results:
             return results[: self.max_results]
         return results
@@ -39,8 +63,52 @@ class searchYouTube:
 
         return full_path
 
-    def parseHTML(self, response):
+    def parseV2(self):
+        base_url = "https://inv.riverside.rocks"
+        searchEndpoint = f"{base_url}/api/v1/search?q={self.search_term}"
+        videos = requests.get(searchEndpoint).json()
+        final = []
+        c = 0
 
+        for video in videos:
+            if c <= self.max_results:
+                if video["type"] == "video":
+                    vID = video["videoId"]
+                    videoId = f"https://www.youtube.com/watch?v={vID}"
+
+                    tmp = {}
+                    metadata = ""
+
+                    tmp["Title"] = video["title"]
+                    metadata += f"Title : {tmp['Title']}\n"
+
+                    tmp["Channel"] = video["author"]
+                    metadata += f"Channel Name : {tmp['Channel']}\n"
+
+                    tmp["Duration"] = self.human_time_duration(video["lengthSeconds"])
+                    metadata += f"Duration : {tmp['Duration']}\n"
+
+                    tmp["Views"] = numerize.numerize(video["viewCount"])
+                    metadata += f"Views : {tmp['Views']}\n"
+
+                    tmp["Uploaded"] = time.strftime(
+                        "%a, %d %b %Y %H:%M:%S %Z", time.localtime(video["published"])
+                    )
+                    metadata += f"Upload Date : {tmp['Uploaded']}\n"
+
+                    tmp["Link"] = videoId
+                    metadata += f"Link : {tmp['Link']}\n"
+
+                    tmp["Thumbnail"] = video["videoThumbnails"][0]["url"]
+
+                    location = self.download(tmp["Thumbnail"], "\n\n" + metadata)
+                    tmp["Location"] = location
+
+                    final.append(tmp)
+                    c += 1
+        return final
+
+    def parseV1(self, response):
         # Get the string only having data of all the videos
         st = response.index("ytInitialData") + len("ytInitialData") + 3
         en = response.index("};", st) + 1
@@ -57,11 +125,9 @@ class searchYouTube:
 
         metadata = ""
         for video in videos:
-
             # Current video master dictionary
             output = {}
             if "videoRenderer" in video.keys():
-
                 # Get current video
                 curr_video = video.get("videoRenderer", {})
 
@@ -124,9 +190,6 @@ class searchYouTube:
                 final.append(output)
         return final
 
-    def get_json(self):
-        return json.dumps({"videos": self.videos})
-
     def get_dict(self):
         return self.videos
 
@@ -138,12 +201,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-q", type=str, default="neovim", help="Query to search.")
 args = parser.parse_args()
 
+# Algorithm to Use
+parser.add_argument("-algo", type=str, default="v1", help="Algo to use v1 v2.")
+args = parser.parse_args()
+
 # Delete Cached Thumbnails
 thumbDir = os.path.expanduser("~/.cache/thumbs")
 os.system(f"rm -rf {thumbDir};mkdir -p {thumbDir}")
 
 # Call the class
-obj = searchYouTube(args.q, 20)
+obj = searchYouTube(args.q, 15, args.algo)
 
 # Get json and store it in the user folder
 jsonString = json.dumps(obj.get_dict(), indent=4)
